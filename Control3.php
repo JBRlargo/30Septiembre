ceaed2f52b128eeca33b7ad0fa306bf1b81a6763
JBRlargo <56647451+JBRlargo@users.noreply.github.com>
2020-09-30 12:19:34 +0200

    Add files via upload

---
+++ Control3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ejercicio 3</title>
+</head>
+<body>
+<?php
+
+$numero=$_POST['numero'];
+$digitos=strlen($numero);
+    print("<p>Hay ".$digitos." dígitos</p>");
+?>
+</form>
+</body>
+</html>
---
+++ Ejercicio3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ejercicio 3</title>
+</head>
+<body>
+<h1>Escriba un número y te digo cuantos dígitos tiene</h1>
+<form action="Control3.php" method="post">
+    <label for="lNumero"></label>
+    <input type="number" value="0" id="numero" name="numero">
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
